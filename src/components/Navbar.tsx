6e8306fbda17b53ead2d4439bb5e5b95686e080e
rahul1172005 <girihitter@gmail.com>
2026-06-27 12:18:11 +0530

    Increase logo size in Navbar to 64px

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,8 +17,8 @@ const Navbar = () => {
                         <Image 
                             src="/images/school_logo.png" 
                             alt="Rhythm Preschool Logo" 
-                            width={48} 
-                            height={48} 
+                            width={64} 
+                            height={64} 
                             style={{ 
                                 objectFit: 'contain',
                                 transform: 'scale(1.0) translate(0px, 0px)', // Keep scale at 1.0 to render original crisp resolution
